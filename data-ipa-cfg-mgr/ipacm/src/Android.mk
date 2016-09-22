4d4f71157e2349c871721ac13000765bf6181dfc
Seraph08 <davidvandenburgh@linux.com>
2016-09-22 13:52:30 -0400

    merge

--- data-ipa-cfg-mgr/ipacm/src/Android.mk
+++ data-ipa-cfg-mgr/ipacm/src/Android.mk
@@ -1,5 +1,3 @@
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
-ifneq (, $(filter aarch64 arm arm64, $(TARGET_ARCH)))
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -12,7 +10,6 @@ LOCAL_C_INCLUDES += external/icu/icu4c/source/common
 else
 LOCAL_C_INCLUDES += external/icu4c/common
 endif
-LOCAL_C_INCLUDES += external/dhcpcd
 LOCAL_C_INCLUDES += external/libxml2/include
 LOCAL_C_INCLUDES += external/libnetfilter_conntrack/include
 LOCAL_C_INCLUDES += external/libnfnetlink/include
@@ -57,8 +54,7 @@ LOCAL_SHARED_LIBRARIES := libipanat
 LOCAL_SHARED_LIBRARIES += libxml2
 LOCAL_SHARED_LIBRARIES += libnfnetlink
 LOCAL_SHARED_LIBRARIES += libnetfilter_conntrack
-LOCAL_SHARED_LIBRARIES += libdhcpcd
-#include $(BUILD_EXECUTABLE)
+include $(BUILD_EXECUTABLE)
 
 ################################################################################
 
@@ -81,7 +77,4 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_MODULE_OWNER := ipacm
-#include $(BUILD_PREBUILT)
-
-endif # $(TARGET_ARCH)
-endif
+include $(BUILD_PREBUILT)
