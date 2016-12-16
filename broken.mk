6fc6c16fd736210253487d5a1a0f68b7ed46eddd
Seraph08 <davidvandenburgh@linux.com>
2016-12-16 07:47:27 -0500

    upgrade upstream
    Signed-off-by: Seraph08 <davidvandenburgh@linux.com>

--- broken.mk
+++ broken.mk
@@ -21,6 +21,7 @@ $(call inherit-product, device/oneplus/oneplus2/device.mk)
 
 # Inherit some common Broken stuff.
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
+$(call inherit-product, vendor/broken/config/common.mk)
 
 PRODUCT_NAME := broken_oneplus2
 PRODUCT_DEVICE := oneplus2
